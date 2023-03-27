25e6ec07a36d633766918b6460e3ac1e4942dbd1
Valentin Merkulov <schnobel@ickis.net>
2023-03-27 09:53:14 +0300

    Added two macro definitions and one macro with SEND_STRING

--- keyboards/feker/ik75/keymaps/schnobel/keymap.c
+++ keyboards/feker/ik75/keymaps/schnobel/keymap.c
@@ -4,24 +4,29 @@ enum layer_names {
     _BASE,
     _FN,
 };
+
+enum custom_keycodes {
+    SCHN_M0 = SAFE_RANGE,
+    SCHN_M1,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_BASE] = LAYOUT(
-                KC_ESC,     G(KC_V),      KC_F2,       KC_F3,     KC_F4,     KC_F5,     KC_F6,      KC_F7,     KC_F8,     KC_F9,       KC_F10,           KC_F11,      KC_F12,     KC_DEL,                   KC_MUTE,
-                KC_GRV,     KC_1,         KC_2,        KC_3,      KC_4,      KC_5,      KC_6,       KC_7,      KC_8,      KC_9,        KC_0,             KC_MINS,     KC_EQL,     KC_BSPC,                  KC_HOME,
-                KC_TAB,     KC_Q,         KC_W,        KC_E,      KC_R,      KC_T,      KC_Y,       KC_U,      KC_I,      KC_O,        KC_P,             KC_LBRC,     KC_RBRC,    KC_BSLS,                  KC_END,
-                KC_CAPS,    KC_A,         KC_S,        KC_D,      KC_F,      KC_G,      KC_H,       KC_J,      KC_K,      KC_L,        KC_SCLN,          KC_QUOT,                 KC_ENT,      KC_PGUP,
-                KC_LSFT,                  KC_Z,        KC_X,      KC_C,      KC_V,      KC_B,       KC_N,      KC_M,      KC_COMM,     KC_DOT,           KC_SLSH,                 KC_RSFT,     KC_UP,       KC_PGDN,
-                KC_LCTL,    KC_LGUI,      KC_LALT,                                      KC_SPC,                           KC_RALT,     LT(_FN,KC_APP),                KC_RCTL,    KC_LEFT,     KC_DOWN,     KC_RIGHT
+                KC_ESC,     G(KC_V),     KC_F2,       KC_F3,       KC_F4,       KC_F5,       SCHN_M1,     KC_F7,       KC_F8,       KC_F9,       KC_F10,          KC_F11,      KC_F12,      KC_DEL,                   KC_MUTE,
+                KC_GRV,     KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_7,        KC_8,        KC_9,        KC_0,            KC_MINS,     KC_EQL,      KC_BSPC,                  KC_HOME,
+                KC_TAB,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,            KC_LBRC,     KC_RBRC,     KC_BSLS,                  KC_END,
+                KC_CAPS,    KC_A,        KC_S,        KC_D,        KC_F,        KC_G,        KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,         KC_QUOT,                  KC_ENT,      KC_PGUP,
+                KC_LSFT,                 KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        KC_N,        KC_M,        KC_COMM,     KC_DOT,          KC_SLSH,                  KC_RSFT,     KC_UP,       KC_PGDN,
+                KC_LCTL,    KC_LGUI,     KC_LALT,                                            KC_SPC,                                KC_RALT,     LT(_FN,KC_APP),               KC_RCTL,     KC_LEFT,     KC_DOWN,     KC_RIGHT
             ),
    
-    /*  Row:    0        1        2        3        4        5        6        7        8        9        10       11       12       13       14       15     */
     [_FN] = LAYOUT(
-                QK_BOOT, KC_MYCM, KC_WHOM, KC_CALC, KC_SLCT, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_MAIL, _______,          RGB_TOG,
-                NK_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,          RGB_MOD,
-                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______,          RGB_HUI,
-                _______, _______, KC_SCRL, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI,
-                _______,          _______, _______, _______, _______, _______, KC_NUM,  _______, _______, _______, _______,          _______, RGB_VAI, RGB_SAD,
-                _______, GUI_TOG, _______,                            _______,                   _______, _______,          _______, _______, RGB_VAD, _______
+                NK_TOGG,    KC_F1,       KC_WHOM,     KC_CALC,     KC_SLCT,     KC_MPRV,     KC_MNXT,     KC_MPLY,     KC_MSTP,     KC_MUTE,     KC_VOLD,         KC_VOLU,     KC_MAIL,     _______,                  RGB_TOG,
+                NK_TOGG,    _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,         RGB_SPD,     RGB_SPI,     _______,                  RGB_MOD,
+                _______,    _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     KC_PSCR,         _______,     _______,     _______,                  RGB_HUI,
+                _______,    _______,     KC_SCRL,     _______,     _______,     _______,     _______,     _______,     _______,     _______,     _______,         _______,                  _______,     RGB_SAI,
+                _______,                 _______,     _______,     _______,     _______,     _______,     KC_NUM,      _______,     _______,     _______,         _______,                  _______,     RGB_VAI,     RGB_SAD,
+                _______,    GUI_TOG,     _______,                                            _______,                               _______,     _______,                      _______,     _______,     RGB_VAD,     _______
             ),
 };
 /*
@@ -59,6 +64,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        case SCHN_M1:
+            if (record->event.pressed) {
+                // SEND_STRING(SS_DOWN(X_LALT) "H" SS_UP(X_LALT) "MC");
+                return false;
+            }
 	}
 
     return true;
@@ -95,6 +105,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_FN]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-
-
-
